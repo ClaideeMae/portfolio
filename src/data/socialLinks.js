df2008bff1463c2e079e9d7d5b38e122cd6a69da
ClaideeMae <claideemae@gmail.com>
2024-03-28 23:10:18 +0800

    ok

---
+++ src/data/socialLinks.js
@@ -0,0 +1,21 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'facebook',
+		url: 'https://www.facebook.com/claideemae.angco07',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/ClaideeMae',
+	},
+	{
+		id: 3,
+		name: 'Instagram',
+		icon: 'instagram',
+		url: '',
+	},
+	
+];
